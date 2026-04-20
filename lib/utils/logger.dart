01e107e94ba712a499d25ecfb92c9dd3c242a573
choi <132165>
2026-04-20 10:41:14 +0900

    � Clean up unnecessary YOLO files

--- lib/utils/logger.dart
+++
@@ -1,9 +0,0 @@
-// Ultralytics 🚀 AGPL-3.0 License - https://ultralytics.com/license
-
-import 'package:flutter/foundation.dart';
-
-void logInfo(String message) {
-  if (kDebugMode) {
-    debugPrint('[YOLO DEBUG] $message');
-  }
-}
